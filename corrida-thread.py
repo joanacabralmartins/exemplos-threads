49f39756f16626d65308832a6bb2c1066a7879a9
Joana Cabral <joanacabralmartins@outlook.com>
2023-08-31 13:53:54 -0300

    funcao para aguardar segundos

--- corrida-thread.py
+++ corrida-thread.py
@@ -1,7 +1,11 @@
 import random
+import time
 
 def gerar_numeros_aleatorios():
     return random.randint(1, 5)
+
+def esperar(segundos):
+    time.sleep(segundos)
 
 def realizar_corrida(id_da_thread):
     posicao_atual = 0
@@ -18,6 +22,8 @@ def realizar_corrida(id_da_thread):
         print(f'Numero sorteado: {passos}')
         print(f'Thread {id_da_thread} Andou {passos} casas')
         print(f'Posição atual da thread {id_da_thread}: {posicao_atual}')
+
+        esperar(1)
 
     print(f'Thread {id_da_thread} - Chegou à posição 50! Total de passos: {total_passos}')
     return total_passos
