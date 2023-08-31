e65caab0e019fbccce76b1d4dff27d55baff8771
Joana Cabral <joanacabralmartins@outlook.com>
2023-08-31 13:51:47 -0300

    funcao para realizar corrida

--- corrida-thread.py
+++ corrida-thread.py
@@ -2,3 +2,22 @@ import random
 
 def gerar_numeros_aleatorios():
     return random.randint(1, 5)
+
+def realizar_corrida(id_da_thread):
+    posicao_atual = 0
+    total_passos = 0
+
+    print(f'Thread {id_da_thread} - Iniciando')
+
+    while posicao_atual < 50:
+        passos = gerar_numeros_aleatorios()
+        total_passos += passos
+        posicao_atual += passos
+
+        print(f'Vez da thread {id_da_thread}')
+        print(f'Numero sorteado: {passos}')
+        print(f'Thread {id_da_thread} Andou {passos} casas')
+        print(f'Posição atual da thread {id_da_thread}: {posicao_atual}')
+
+    print(f'Thread {id_da_thread} - Chegou à posição 50! Total de passos: {total_passos}')
+    return total_passos
