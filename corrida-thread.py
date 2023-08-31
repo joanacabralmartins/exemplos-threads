3f06637b4fbd7138f8d2612d5eb13e57c270645f
Joana Cabral <joanacabralmartins@outlook.com>
2023-08-31 13:58:05 -0300

    iniciar corrida

--- corrida-thread.py
+++ corrida-thread.py
@@ -1,5 +1,6 @@
 import random
 import time
+import threading
 
 def gerar_numeros_aleatorios():
     return random.randint(1, 5)
@@ -27,3 +28,27 @@ def realizar_corrida(id_da_thread):
 
     print(f'Thread {id_da_thread} - Chegou à posição 50! Total de passos: {total_passos}')
     return total_passos
+
+def iniciar_corrida(num_threads):
+    threads = []
+    resultados = []
+
+    def thread_secundaria(id_da_thread):
+        total_passos = realizar_corrida(id_da_thread)
+        resultados.append(total_passos)
+
+    for i in range(num_threads):
+        thread = threading.Thread(target=thread_secundaria, args=(i,))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    indice_vencedor = resultados.index(min(resultados))
+    id_da_thread_vencedora = indice_vencedor + 1
+    print(f'Thread {id_da_thread_vencedora} venceu com {resultados[indice_vencedor]} passos')
+
+if __name__ == "__main__":
+    num_threads = 2
+    iniciar_corrida(num_threads)
